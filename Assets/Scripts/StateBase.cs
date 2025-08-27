52776c710f905d35f293693212e2104c646dd034
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-08-27 09:30:36 -0300

    state machine pt1

---
+++ Assets/Scripts/StateBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateBase 
+{
+    public virtual void OnStateEnter()
+    {
+        Debug.Log("onStateEnter");
+    }
+
+    public virtual void OnStateStay()
+    {
+        Debug.Log("onStateStay");
+    }
+
+    public virtual void OnStateExit()
+    {
+        Debug.Log("onStateExit");
+    }
+}
+
