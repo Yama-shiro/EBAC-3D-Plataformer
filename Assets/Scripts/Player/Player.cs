340cb5c853aa6c361fc6c39735e051639e04fcf3
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-09-01 08:55:49 -0300

    pular

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+   
+
     public Animator animator;
 
     public CharacterController characterController;
@@ -11,6 +13,9 @@ public class Player : MonoBehaviour
     public float turnSpeed = 1f;
     public float gravity = 9.8f;
     private float vSpeed = 0f;
+    public float jumpSpeed = 15f;
+
+    public KeyCode jumpKeyCode = KeyCode.Space;
 
     void Update()
     {
@@ -19,7 +24,17 @@ public class Player : MonoBehaviour
         var inputAxisVertical = Input.GetAxis("Vertical");
         var speedVector = transform.forward * inputAxisVertical * speed;
 
-        vSpeed = gravity * Time.deltaTime;
+        if (characterController.isGrounded)
+        {
+            vSpeed = 0;
+            if (Input.GetKeyDown(jumpKeyCode))
+            {
+                vSpeed = jumpSpeed;
+            }
+        }
+
+         
+        vSpeed -= gravity * Time.deltaTime;
         speedVector.y = vSpeed;
 
         characterController.Move(speedVector * Time.deltaTime);
