5c0d67235f733778b5823b01a1ab38d79e5f67db
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-09-04 10:00:07 -0300

    inimigo collider

--- Assets/Scripts/Gun/ProjectileBase.cs
+++ Assets/Scripts/Gun/ProjectileBase.cs
@@ -19,9 +19,13 @@ public class ProjectileBase : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter(Collider collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        var damageable = collision.transform.GetComponent<IDamageable>();
+
+        if (damageable != null) damageable.Damage(damageAmount);
+
+        Destroy(gameObject);
     }
 }
 
